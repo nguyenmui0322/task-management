caa19182220ea33ce0176a4bf4adaa8dc6a68649
mui <nvanmui8@gmail.com>
2024-03-23 00:44:35 +0700

    search

--- api/v1/controllers/task.controller.js
+++ api/v1/controllers/task.controller.js
@@ -1,6 +1,7 @@
 const Task = require("../models/task.model");
 
 const paginationHelper = require("../../../helpers/pagination");
+const searchHelper = require("../../../helpers/search");
 
 // [GET] /api/v1/tasks
 module.exports.index = async (req, res) => {
@@ -12,7 +13,14 @@ module.exports.index = async (req, res) => {
   if (req.query.status) {
     find.status = req.query.status;
   }
-  // Hết Filter
+  // End Filter
+
+  // Search
+  const objectSearch = searchHelper(req.query);
+  if (objectSearch.regex) {
+    find.title = objectSearch.regex;
+  }
+  // End Search
 
   // Pagination
   const countTasks = await Task.countDocuments(find);
