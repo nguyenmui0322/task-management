8859ca5e8c1338deb1e7c67106b62295d20af610
mui <nvanmui8@gmail.com>
2024-03-23 17:48:38 +0700

    login

--- api/v1/controllers/user.controller.js
+++ api/v1/controllers/user.controller.js
@@ -36,3 +36,39 @@ module.exports.register = async (req, res) => {
     token: token,
   });
 };
+
+// [POST] /api/v1/users/login
+module.exports.login = async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const user = await User.findOne({
+    email: email,
+    deleted: false,
+  });
+
+  if (!user) {
+    res.json({
+      code: 400,
+      message: "Email khong ton tai",
+    });
+    return;
+  }
+
+  if (md5(password) !== user.password) {
+    res.json({
+      code: 400,
+      message: "Sai mat khau",
+    });
+    return;
+  }
+
+  const token = user.token;
+  res.cookie("token", token);
+
+  res.json({
+    code: 200,
+    message: "Dang nhap thanh cong",
+    token: token,
+  });
+};
